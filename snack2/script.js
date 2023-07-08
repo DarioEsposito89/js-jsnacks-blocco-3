984078800fa9bcc6496795c2c7c36d5473f6f11a
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-08 16:05:20 +0200

    inserito la descrizione dell'esercizio

--- snack2/script.js
+++ snack2/script.js
@@ -1,3 +1,6 @@
+// Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l’altro di verde. Partendo da un array di numeri, 
+// stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
 // Array vuoto di numeri casuali
 const numbersArray = [];
 console.log(numbersArray);
